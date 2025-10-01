1566640491fe023c6ba72e7cd64d28a668bf5e03
saracarretero <carreteroprogramar@gmail.com>
2025-09-30 21:01:04 -0300

    Creación de los mensajes (iterando un array) en el chat

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,6 +1,21 @@
+import avatar from "../assets/avatar.jpeg"
+import "../styles/components/ChatHeader.css"
+
 const ChatHeader = () => {
   return (
-    <h2>Funciona</h2>
+    <header className="main-header">
+      <div className="chat-header">
+        <img src={avatar} alt="imagen de perfil" />
+        <h3>Aiden Chavez</h3>
+        <p>Last seen: 2 hours ago</p>
+      </div>
+      <div className="chat-actions">
+        <button title="Tomar una foto">📷</button>
+        <button title="Enviar una foto">🖼️</button>
+        <button title="Configuración">⚙️</button>
+        <button title="Ayuda">❓</button>
+      </div>
+    </header>
   )
 }
 
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,7 +1,20 @@
-const MessageList = () => {
-  return (
-    <h2>Funciona</h2>
-  )
-}
+import Message from "./Message";
 
-export default MessageList
+const messages = [
+  { id: 1, text: "Hi Aiden, how are you? How is the project coming along?", sender: "sent", time: "10:10 AM, Today" },
+  { id: 2, text: "Are we meeting today?", sender: "received", time: "10:12 AM, Today" },
+  { id: 3, text: "Project has been already finished and I have results to show you.", sender: "received", time: "10:15 AM, Today" },
+];
+
+export default function MessageList() {
+  return (
+    <section className="chat-messages">
+      {messages.map(message => (
+        <div className={`message-${message.sender}`}>
+          <p>{message.text}</p>
+          <p className="timestamp">{message.time}</p>
+        </div>
+      ))}
+    </section>
+  );
+}
