cdbc84304537a2b2936a307710da5c9b724e0c41
saracarretero <carreteroprogramar@gmail.com>
2025-09-30 22:11:33 -0300

    algoritmo en formLogin, que sirve para que el formulario de iniciar sesión envié el dato ingresado

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,6 +1,15 @@
 const FormLogin = () => {
+
+  //manejadorDeEnvio por lo general se lo nombra como handleSubmit
+  const manejadorDeEnvio = (e) => {
+
+    //e.preventDefault () método para prevenir el comportamiento por defecto que tiene el formulario, es decir, tiene por defecto que cada vez que se envíe, se recarga la página.
+    e.preventDefault()
+  }
+
   return (
-    <form>
+
+    <form onSubmit={manejadorDeEnvio}>
       <label htmlFor="">Contraseña de acceso</label>
       <input type="password" name="" id="" />
       <button>Acceder</button>
